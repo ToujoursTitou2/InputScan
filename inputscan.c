c9fb5de0aed29ed4fb2f3ad6bd4b80c36295d960
Titou <129084186+ToujoursTitou2@users.noreply.github.com>
2023-05-21 09:56:42 +0200

    Update inputscan.c

--- inputscan.c
+++ inputscan.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 
 char inputScan(char *input, int size){
-  fgets(input, size, stdin);
-  if(input[strlen(input) - 1] == '\n'){
-    input[strlen(input) -1] = '\0';
+  if(fgets(input, size, stdin) != NULL){;
+    if(input[strlen(input) - 1] == '\n'){
+      input[strlen(input) - 1] = '\0';
+    }
+    return *input;
+  }else{
+    printf("Error: %s\n", strerror(errno));
+    exit(EXIT_FAILURE);
   }
-  return *input;
 }
