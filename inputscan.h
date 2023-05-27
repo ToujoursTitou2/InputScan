4a10047bf528ae9ed14914b67b5e8b5a2f99cbea
Titou <129084186+ToujoursTitou2@users.noreply.github.com>
2023-05-27 13:50:41 +0200

    Update inputscan.h

--- inputscan.h
+++ inputscan.h
@@ -1,5 +1,10 @@
 #ifndef INPUTSCAN_H
 #define INPUTSCAN_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 void clearBuffer();
-char inputScan(char *input, int size);
+char charScan(char *input, int size);
+int intScan(int size);
+double doubleScan(int size);
 #endif
